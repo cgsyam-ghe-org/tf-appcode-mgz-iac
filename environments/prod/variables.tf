a7aa491df35c1672db62b8c396e104e17cf7d438
Syam Sankar <syamcg@google.com>
2024-05-23 17:08:39 +0530

    network initial commit (#1)

--- environments/prod/variables.tf
+++ environments/prod/variables.tf
@@ -1,8 +1,30 @@
-/*
-# Example
-
-variable "config" {
-    type        = string
-    description = "This is the config variable"
+variable "devops_vpc" {
+  description = "Properties of the Shared VPC network to be created"
+  type = object({
+    project_id                             = string
+    network_name                           = string
+    routing_mode                           = string
+    delete_default_internet_gateway_routes = bool
+    subnets = list(object(
+      {
+        subnet_name               = string,
+        subnet_ip                 = string,
+        subnet_region             = string,
+        subnet_private_access     = string,
+        subnet_flow_logs          = optional(string)
+        subnet_flow_logs_interval = optional(string)
+        subnet_flow_logs_sampling = optional(number)
+        subnet_flow_logs_metadata = optional(string)
+        purpose                   = optional(string)
+        role                      = optional(string)
+      })
+    )
+    secondary_ranges = map(list(object({ range_name = string, ip_cidr_range = string })))
+    shared_vpc_host  = bool
+  })
 }
-*/
+
+variable "cloud_nat_devops_uc1" {
+  type        = string
+  description = "Cloud NAT name"
+}
